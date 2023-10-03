0ff1995f65ce06da188d7d6f8376175ae16df89a
annagarmiza <annagarmiza@gmail.com>
2023-10-02 21:50:17 -0400

    updated question 2038

--- leetcode_daily_challenge/2038.Remove_Colored_Pieces_if_Both_Neighbors_are_the_same_color.js
+++ leetcode_daily_challenge/2038.Remove_Colored_Pieces_if_Both_Neighbors_are_the_same_color.js
@@ -1,21 +1,20 @@
 /*
 check the count of all "AAA"
 check the count of all "BBB"
-time complexity: O(N)
+n = length of colors
+time complexity: O(n)
 space complexity: O(1)
 */
 
 const winnerOfGame = (colors) => {
   if (colors.length < 3) return false;
+
   let countAlice = 0;
   let countBob = 0;
 
   for (let i = 1; i < colors.length - 1; i++) {
-    if (colors[i - 1] === "A" && colors[i] === "A" && colors[i + 1] === "A") {
-      countAlice++;
-    }
-    if (colors[i - 1] === "B" && colors[i] === "B" && colors[i + 1] === "B") {
-      countBob++;
+    if (colors[i - 1] === colors[i] && colors[i] === colors[i + 1]) {
+      colors[i] === "A" ? countAlice++ : countBob++;
     }
   }
 
